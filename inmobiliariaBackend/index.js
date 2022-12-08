6f603be20f6e49eaa5c174f29d1059a01506638e
Pablo de Leon <pablo.dl@outlook.com>
2022-12-08 18:00:11 -0300

    backend endpoints etc

---
+++ inmobiliariaBackend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const morgan = require("morgan");
+
+const bodyParser = require("body-parser");
+
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+
+require("dotenv").config();
+
+const inmueblesRoutes = require("./routes/inmuebles");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/error", (req, res) => {
+  res.status(400).json({ error: "Error inesperado" });
+});
+
+app.use("/api", inmueblesRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
